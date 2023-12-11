eea787bd606d5067d04aa755d322f8a460a5f7b5
Arkadii Serkov <serkov_a@auca.kg>
2023-12-11 21:46:52 +0600

    add img folder and create navigation (USP)

---
+++ src/routes/USP/+page.svelte
@@ -0,0 +1,15 @@
+<div>1.)What is the purpose of the landing page (your site)?</div>
+
+<div>1.)Landing page aims to inform and showcase the Porsche 911 Turbo S, emphasizing its features, rich history, and compelling reasons to choose Porsche.</div>
+
+<div>2.)Who is your target audience?</div>
+
+<div>2.)Encompassing car enthusiasts, potential buyers, and devoted fans of the Porsche brand, the target audience is individuals passionate about luxury sports cars, especially the Porsche 911 Turbo S.</div>
+
+<div>3.)What is the call to action (CTA)?</div>
+
+<div>3.)Encouraging visitors to "Explore Now," the CTA directs them to the Porsche configurator for a personalized experience.</div>
+
+<div>4.)What is you unique selling proposition?</div>
+
+<div>4.)Help you discover the ideal car that aligns with your preferences, delivering an unforgettable experience through its quality, dynamic performance, striking aesthetics, and rich heritage.</div>
